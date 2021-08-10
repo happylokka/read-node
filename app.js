a4d6c25ea71353316f4192aca0e2a32160c50798
happylokka <810321302@qq.com>
2021-08-11 01:01:44 +0800

    feat: 更新

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const router = require('./router')
+const fs = require('fs')
+const https = require('https')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
 app.use('/', router)
 
-const server = app.listen(3000, function() {
-    const { address, port } = server.address()
-    console.log('HTTPS Server is running on: https://localhost:', address, port)
+const privateKey = fs.readFileSync('./https/dsnztgz.key', 'utf8')
+const pem = fs.readFileSync('./https/dsnztgz.pem', 'utf8')
+const credentials = { key: privateKey, cert: pem }
+const httpsServer = https.createServer(credentials, app)
+const SSLPORT = 18082
+httpsServer.listen(SSLPORT, function() {
+  console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT)
 })
--- router/user.js
+++ router/user.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const Result = require('../models/Result')
+const { querySql } = require('../db')
 
 const router = express.Router()
+
+router.post('/login', function(req, res, next) {
+  const { username, password } = req.body
+  querySql('select * from admin_user').then(results => {
+    console.log(results)
+  }).catch(err => {
+    console.log(err)
+  })
+  if (username === 'admin' && password === '111111') {
+    new Result('登录成功').success(res)
+  } else {
+    new Result('登录失败').fail(res)
+  }
+})
 
 router.get('/info', function(req, res, next) {
   res.json('user info...')
