ea87a6a5f2f6211226e711c8bb2d0581a615c475
happylokka <810321302@qq.com>
2021-10-01 14:32:25 +0800

    feat: 更新

--- db/index.js
+++ db/index.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql')
-const config = require('config')
+const config = require('./config')
+const { debug } = require('../utils/constant')
 
 function connect () {
     return mysql.createConnection({
@@ -11,25 +12,45 @@ function connect () {
     })
 }
 
-function querySql (sql) {
+function querySql(sql) {
     const conn = connect()
+    debug && console.log(sql)
     return new Promise((resolve, reject) => {
-        try {
-            conn.query(sql, (err, results) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(results)
-                }
-            })
-        } catch (e) {
-            reject(e)
-        } finally {
-            conn.end()
-        }
+      try {
+        conn.query(sql, (err, results) => {
+          if (err) {
+            debug && console.log('查询失败，原因:' + JSON.stringify(err))
+            reject(err)
+          } else {
+            debug && console.log('查询成功', JSON.stringify(results))
+            resolve(results)
+          }
+        })
+      } catch (e) {
+        reject(e)
+      } finally {
+        conn.end()
+      }
     })
-}
+  }
+
+  function queryOne(sql) {
+    return new Promise((resolve, reject) => {
+      querySql(sql)
+        .then(results => {
+          if (results && results.length > 0) {
+            resolve(results[0])
+          } else {
+            resolve(null)
+          }
+        })
+        .catch(error => {
+          reject(error)
+        })
+    })
+  }
 
 module.exports = {
-
+    querySql,
+    queryOne
 }
--- router/user.js
+++ router/user.js
@@ -1,25 +1,57 @@
 const express = require('express')
 const Result = require('../models/Result')
-const { querySql } = require('../db')
+const { login, findUser } = require('../services/user')
+const { md5, decoded } = require('../utils')
+const { PWD_SALT, PRIVATE_KEY, JWT_EXPIRED } = require('../utils/constant')
+const { body, validationResult } = require('express-validator')
+const boom = require('boom')
+const jwt = require('jsonwebtoken')
 
 const router = express.Router()
 
-router.post('/login', function(req, res, next) {
-  const { username, password } = req.body
-  querySql('select * from admin_user').then(results => {
-    console.log(results)
-  }).catch(err => {
-    console.log(err)
-  })
-  if (username === 'admin' && password === '111111') {
-    new Result('登录成功').success(res)
-  } else {
-    new Result('登录失败').fail(res)
-  }
-})
+router.post(
+  '/login',
+  [
+    body('username').isString().withMessage('username类型不正确'),
+    body('password').isString().withMessage('password类型不正确')
+  ],
+  function(req, res, next) {
+    const err = validationResult(req)
+    if (!err.isEmpty()) {
+      const [{ msg }] = err.errors
+      next(boom.badRequest(msg))
+    } else {
+      const username = req.body.username
+      const password = md5(`${req.body.password}${PWD_SALT}`)
 
-router.get('/info', function(req, res, next) {
-  res.json('user info...')
+      login(username, password).then(user => {
+        if (!user || user.length === 0) {
+          new Result('登录失败').fail(res)
+        } else { 
+          const token = jwt.sign(
+          { username },
+          PRIVATE_KEY,
+          { expiresIn: JWT_EXPIRED }
+        )
+          new Result({ token }, '登录成功').success(res)
+        }
+      })
+    }
+  })
+
+router.get('/info', function (req, res,) {
+  const decode = decoded(req)
+  if (decode && decode.username) {
+    findUser(decode.username).then(user => {
+      if (user) {
+        user.roles = [user.role]
+        new Result(user, '用户信息查询成功').success(res)
+      } else {
+        new Result(user, '用户信息查询失败').fail(res)
+      }
+    })
+  }
+  
 })
 
 module.exports = router
